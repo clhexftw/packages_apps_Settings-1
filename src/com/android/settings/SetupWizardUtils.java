LineageOS/android_packages_apps_Settings

Settings: update SUW theming for 12
Change-Id: I2f495b9f4060a3d978666a8b190a47e58c05847e

src/com/android/settings/SetupWizardUtils.java
@@ -88,7 +88,7 @@ public class SetupWizardUtils {
                 }
             }
         }
-        return R.style.GlifV3Theme;
+        return R.style.SudDynamicColorThemeSettings_SetupWizard;
     }
 
     public static int getTransparentTheme(Context context, Intent intent) {

clhexftw/packages_apps_Settings-1

src/com/android/settings/SetupWizardUtils.java
@@ -88,7 +88,7 @@ public class SetupWizardUtils {
                 }
             }
         }
-        return R.style.GlifTheme;
+        return R.style.SudDynamicColorThemeSettings_SetupWizard_DayNight;
     }
 
     public static int getTransparentTheme(Context context, Intent intent) {